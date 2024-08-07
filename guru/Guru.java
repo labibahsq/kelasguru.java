6ab25c27a681b81ed0afba5b2ce010bfcf269a08
labibahsq <labibah_qothrunnada_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:32:11 +0700

    Add files via upload

---
+++ guru/Guru.java
@@ -0,0 +1,17 @@
+//Object Class
+public class Guru {
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    /*Constractor nama sama dengan nama Class
+     */
+    public Guru() {
+        id = 0;
+        nama = "";
+        mapel = "";
+        alamat = "";
+    }
+    
+}
---
+++ guru/ObjectGuru.java
@@ -0,0 +1,61 @@
+//Driver Classs
+public class ObjectGuru {
+    public static void main(String[] args) {
+        Guru pasha = new Guru();
+        Guru feni = new Guru();
+        Guru diaur = new Guru();
+        Guru aulia = new Guru();
+        Guru rara = new Guru();
+
+        pasha.id = 1;
+        pasha.nama = "Pashatania Fitri Indah Lestari, S.Kom.";
+        pasha.mapel = "Produktif RPL";
+        pasha.alamat = "Malang";
+        System.out.println("Ini Data Pasha");
+        System.out.println(pasha.id);
+        System.out.println(pasha.nama);
+        System.out.println(pasha.mapel);
+        System.out.println(pasha.alamat);
+
+        feni.id = 2;
+        feni.nama = "Feniliya Mayrini";
+        feni.mapel = "Matematika";
+        feni.alamat = "Malang";
+        System.out.println("Ini Data Feni");
+        System.out.println(feni.id);
+        System.out.println(feni.nama);
+        System.out.println(feni.mapel);
+        System.out.println(feni.alamat);
+
+        diaur.id = 3;
+        diaur.nama = "Diaur Rahman, S.Pd.";
+        diaur.mapel = "Ipas, Olah Raga";
+        diaur.alamat = "Malang";
+        System.out.println("Ini Data Diaur");
+        System.out.println(diaur.id);
+        System.out.println(diaur.nama);
+        System.out.println(diaur.mapel);
+        System.out.println(diaur.alamat);
+
+        aulia.id = 4;
+        aulia.nama = "Aulia Mas'adah, S.Kom.";
+        aulia.mapel = "Produktif RPL";
+        aulia.alamat = "Malang";
+        System.out.println("Ini Data Aulia");
+        System.out.println(aulia.id);
+        System.out.println(aulia.nama);
+        System.out.println(aulia.mapel);
+        System.out.println(aulia.alamat);
+
+        rara.id = 5;
+        rara.nama = "Ria Cahya Kurnia Rahma, S.Pd.";
+        rara.mapel = "Pendidikan Pancasila dan Kewarganegaraan";
+        rara.alamat = "Malang";
+        System.out.println("Ini Data Rara");
+        System.out.println(rara.id);
+        System.out.println(rara.nama);
+        System.out.println(rara.mapel);
+        System.out.println(rara.alamat);
+
+    }
+}
